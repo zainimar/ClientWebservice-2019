d07615b9abfdd292e0f1691372057e64305816a6
Zainimar <zainimm@gmail.com>
2019-02-27 16:40:22 +0800

    client

--- client_detail.php
+++ client_detail.php
@@ -14,14 +14,15 @@
 		//call url page web service api
 		$url_api = 'http://localhost:8080/webservice/sakila/film_detail.php';
 		$id = $_POST['idd'];
-		$param = ['query' => ['idd' => $id]];
+		//kena pass token di sini
+		$param = ['query' => ['idd' => $id, 'token' => 'wssdff222555555eedffg']];
 
 		$result = $client->request('GET', $url_api, $param);
 
 		$detail_film = json_decode($result->getBody()->getContents());
 		if (isset($detail_film->err))
 		{
-			echo "Tiada Rekod";
+			echo $detail_film->err;
 		} 
 		else
 		{
